be3714b539f4716a2fccedbe84ba214c3976075d
atulbanwar <atulbanwar@gmail.com>
2017-04-01 19:30:41 -0400

    Reading Javascript file

--- JavaScriptAnalyzer/Program.cs
+++ JavaScriptAnalyzer/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace JavaScriptAnalyzer
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string fileName;
+
+			Console.Write("Enter JavaScript file name with extension (or full file path): ");
+			fileName = Console.ReadLine();
+
+			if (Util.isValidFile(fileName))
+			{
+				string line;
+
+				StreamReader file = new StreamReader(fileName);
+				while ((line = file.ReadLine()) != null)
+				{
+					Console.WriteLine(line);
+				}
+
+				file.Close();
+			}
+
+			Console.ReadLine();
+		}
+	}
 }
---
+++ JavaScriptAnalyzer/Util.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace JavaScriptAnalyzer
+{
+	class Util
+	{
+		public static bool isValidFile(string fileName)
+		{
+			if (String.IsNullOrWhiteSpace(fileName))
+			{
+				Console.WriteLine("Please enter valid file name.");
+				return false;
+			}
+
+			if (!Path.GetExtension(fileName).Equals(".js"))
+			{
+				Console.WriteLine("The input file is not a valid javascript file.");
+				return false;
+			}
+
+			if (!File.Exists(fileName))
+			{
+				Console.WriteLine("File not found. Please enter valid file name with extension (or full file path).");
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
